42c334458bbbe0b473283b6fc6fa8f2397081abb
highf <412704048@qq.com>
2019-06-25 13:52:41 +0800

    css和css预处理
    css和css预处理

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import $ from "jquery"
 $("ul li:even").css({background:'red'})
-$("ul li:odd").css({background:'pink'})
+$("ul li:odd").css({background:'purple'})
+
+import './index.css'
+import './index.less'
+import './a.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path  = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry:path.join(__dirname,'./src/main.js'),
   output:{
     path:path.join(__dirname,'./dist'),
     filename:'bundle.js'
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname,'./src/index.html'),
+      filename: 'index.html'
+    })
+  ],
+  module:{
+    rules:[
+      {test:/\.css$/,use:['style-loader','css-loader']},
+      {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+      {test:/\.scss$/,use:['style-loader','css-loader','sass-loader']},
+    ]
   }
 }
